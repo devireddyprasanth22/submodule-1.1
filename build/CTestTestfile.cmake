a2b5b59915f2c8532c42a4156f18440c013498d5
devireddyprasanth22 <dpkrdrkr@gmail.com>
2025-03-23 14:55:41 +0800

    updated cmakefile that exports module as library

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dp/Desktop/speqtral_challenge/master-software/submodule-1/submodule-1.1
-# Build directory: /Users/dp/Desktop/speqtral_challenge/master-software/submodule-1/submodule-1.1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(submodule11 "/Users/dp/Desktop/speqtral_challenge/master-software/submodule-1/submodule-1.1/build/test-submodule11")
-set_tests_properties(submodule11 PROPERTIES  _BACKTRACE_TRIPLES "/Users/dp/Desktop/speqtral_challenge/master-software/submodule-1/submodule-1.1/CMakeLists.txt;18;add_test;/Users/dp/Desktop/speqtral_challenge/master-software/submodule-1/submodule-1.1/CMakeLists.txt;0;")
